dc57ff31bd4676f2f595d4d276099b5b60dd762b
Barros9 <giu.barreca@gmail.com>
2024-12-01 12:07:28 +0100

    Day01 - Part 2

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -7,24 +7,29 @@ import kotlin.math.abs
 fun main() {
     fun part1(input: List<String>): Int {
         val (firstArray, secondArray) = input
-            .map { it.split("   ").map(String::toInt) }
-            .let { pairs -> pairs.map { it[0] } to pairs.map { it[1] } }
+            .map { line -> line.split("   ").map(String::toInt) }
+            .let { pairs -> pairs.map { it[0] }.sorted() to pairs.map { it[1] }.sorted() }
 
-        val sortedFirst = firstArray.sorted()
-        val sortedSecond = secondArray.sorted()
-
-        return sortedFirst
-            .zip(sortedSecond)
+        return firstArray
+            .zip(secondArray)
             .sumOf { (valueFirst, valueSecond) -> abs(valueFirst - valueSecond) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (firstArray, secondArray) = input
+            .map { line -> line.split("   ").map(String::toInt) }
+            .let { pairs -> pairs.map { it[0] } to pairs.map { it[1] } }
+
+        val countMap = secondArray.groupingBy { it }.eachCount()
+
+        return firstArray.sumOf { valueFirst ->
+            valueFirst * (countMap[valueFirst] ?: 0)
+        }
     }
 
     val testInput = readInput("day01/Day01_test")
     check(part1(testInput) == 11)
-//    check(part2(testInput) == 1)
+    check(part2(testInput) == 31)
 
     val input = readInput("day01/Day01")
     part1(input).println()
