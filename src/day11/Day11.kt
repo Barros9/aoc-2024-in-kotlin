2aa98bff9c9c3421218d0aad5baf0a8683686829
Barros9 <giu.barreca@gmail.com>
2024-12-13 13:45:35 +0100

    Day11 - Part 2

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -2,6 +2,7 @@ package day11
 
 import println
 import readInput
+import java.math.BigInteger
 
 fun main() {
     fun part1(input: List<String>): Int {
@@ -14,13 +15,18 @@ fun main() {
         return stones.size
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): BigInteger {
+        var stoneCounts = input[0].split(" ").groupingBy { it }.eachCount().mapValues { it.value.toBigInteger() }
+
+        repeat(75) {
+            stoneCounts = transformStonesCounts(stoneCounts)
+        }
+
+        return stoneCounts.values.reduce(BigInteger::add)
     }
 
     val testInput = readInput("day11/Day11_test")
     check(part1(testInput) == 55312)
-    check(part2(testInput) == 0)
 
     val input = readInput("day11/Day11")
     part1(input).println()
@@ -47,4 +53,32 @@ private fun transformStones(stones: List<String>): List<String> {
     }
 
     return newStones
+}
+
+private fun transformStonesCounts(stones: Map<String, BigInteger>): Map<String, BigInteger> {
+    val newCounts = mutableMapOf<String, BigInteger>()
+
+    for ((stone, count) in stones) {
+        when {
+            stone == "0" -> {
+                newCounts["1"] = newCounts.getOrDefault("1", BigInteger.ZERO) + count
+            }
+
+            stone.length % 2 == 0 -> {
+                val mid = stone.length / 2
+                val left = stone.substring(0, mid).trimStart('0').ifEmpty { "0" }
+                val right = stone.substring(mid).trimStart('0').ifEmpty { "0" }
+
+                newCounts[left] = newCounts.getOrDefault(left, BigInteger.ZERO) + count
+                newCounts[right] = newCounts.getOrDefault(right, BigInteger.ZERO) + count
+            }
+
+            else -> {
+                val newStone = (stone.toBigInteger() * BigInteger.valueOf(2024)).toString()
+                newCounts[newStone] = newCounts.getOrDefault(newStone, BigInteger.ZERO) + count
+            }
+        }
+    }
+
+    return newCounts
 }
