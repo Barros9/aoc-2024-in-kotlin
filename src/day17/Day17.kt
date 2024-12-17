817c75eef26c9dd7c829b4dab81038d3c7bf77d1
Barros9 <giu.barreca@gmail.com>
2024-12-17 15:19:41 +0100

    Day17 - Part 2 - optimization

--- src/day17/Day17.kt
+++ src/day17/Day17.kt
@@ -10,41 +10,31 @@ fun main() {
         return output.joinToString(",")
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>): Long {
         val (_, program) = parseInput(input)
-
-        var a = 1
-        while (true) {
-            val registers = mutableListOf(a, 0, 0)
-            val output = runProgram(registers, program)
-
-            if (output == program) {
-                return a
-            }
-            a++
-        }
+        return decipher(0L, program, program.size - 1)
     }
 
     val testInput1 = readInput("day17/Day17_test_1")
     check(part1(testInput1) == "4,6,3,5,6,3,5,2,1,0")
 
     val testInput2 = readInput("day17/Day17_test_2")
-    check(part2(testInput2) == 117440)
+    check(part2(testInput2) == 117440L)
 
     val input = readInput("day17/Day17")
     part1(input).println()
     part2(input).println()
 }
 
-private fun parseInput(input: List<String>): Pair<MutableList<Int>, List<Int>> {
-    val registers = mutableListOf(0, 0, 0)
+private fun parseInput(input: List<String>): Pair<MutableList<Long>, List<Int>> {
+    val registers = mutableListOf(0L, 0L, 0L)
     var programLine = ""
 
     for (line in input) {
         when {
-            line.startsWith("Register A:") -> registers[0] = line.split(": ")[1].toInt()
-            line.startsWith("Register B:") -> registers[1] = line.split(": ")[1].toInt()
-            line.startsWith("Register C:") -> registers[2] = line.split(": ")[1].toInt()
+            line.startsWith("Register A:") -> registers[0] = line.split(": ")[1].toLong()
+            line.startsWith("Register B:") -> registers[1] = line.split(": ")[1].toLong()
+            line.startsWith("Register C:") -> registers[2] = line.split(": ")[1].toLong()
             line.startsWith("Program:") -> programLine = line.split(": ")[1]
         }
     }
@@ -53,41 +43,51 @@ private fun parseInput(input: List<String>): Pair<MutableList<Int>, List<Int>> {
     return Pair(registers, program)
 }
 
-private fun runProgram(registers: MutableList<Int>, program: List<Int>): List<Int> {
+private fun runProgram(registers: MutableList<Long>, program: List<Int>): List<Int> {
     var (a, b, c) = registers
     var ip = 0
     val output = mutableListOf<Int>()
 
-    while (ip < program.size) {
-        val opcode = program[ip]
-        val operand = program[ip + 1]
-
-        when (opcode) {
-            0 -> a /= 1 shl comboValue(operand, a, b, c)  // adv
-            1 -> b = b xor operand  // bxl
-            2 -> b = comboValue(operand, a, b, c) % 8  // bst
-            3 -> {  // jnz
-                if (a != 0) {
-                    ip = operand
-                    continue
-                }
-            }
-            4 -> b = b xor c  // bxc
-            5 -> output.add(comboValue(operand, a, b, c) % 8)  // out
-            6 -> b = a / (1 shl comboValue(operand, a, b, c))  // bdv
-            7 -> c = a / (1 shl comboValue(operand, a, b, c))  // cdv
-            else -> throw IllegalArgumentException("Unknown command: $opcode")
+    fun getOperandValue(operand: Int): Long {
+        return when (operand) {
+            in 0..3 -> operand.toLong()
+            4 -> a
+            5 -> b
+            6 -> c
+            else -> -1
         }
+    }
+
+    while (ip < program.size) {
+        val op = program[ip]
+        val operand = program[ip + 1]
+        val co = getOperandValue(operand)
         ip += 2
+
+        when (op) {
+            0 -> a /= 1 shl co.toInt() // adv
+            1 -> b = b xor operand.toLong() // bxl
+            2 -> b = co % 8 // bst
+            3 -> if (a != 0L) ip = operand // jnz
+            4 -> b = b xor c // bxc
+            5 -> output.add((co % 8).toInt()) // out
+            6 -> b = a / (1 shl co.toInt()) // bdv
+            7 -> c = a / (1 shl co.toInt()) // cdv
+            else -> throw IllegalArgumentException("Unknown command: $op")
+        }
     }
     return output
 }
 
-private fun comboValue(operand: Int, a: Int, b: Int, c: Int): Int =
-    when (operand) {
-        in 0..3 -> operand
-        4 -> a
-        5 -> b
-        6 -> c
-        else -> throw IllegalArgumentException("Not valid: $operand")
+private fun decipher(subA: Long, program: List<Int>, left: Int): Long {
+    if (left < 0) return subA
+    for (i in 0..7) {
+        val a = subA * 8 + i
+        val output = runProgram(mutableListOf(a, 0L, 0L), program)
+        if (output == program.subList(left, program.size)) {
+            val result = decipher(a, program, left - 1)
+            if (result != 0L) return result
+        }
     }
+    return 0L
+}
