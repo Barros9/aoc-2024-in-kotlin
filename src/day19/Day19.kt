41311c4a1f81f657a06625e4d904e4983bf89437
Barros9 <giu.barreca@gmail.com>
2024-12-19 16:42:13 +0100

    Day19 - Part 2

--- src/day19/Day19.kt
+++ src/day19/Day19.kt
@@ -18,10 +18,22 @@ fun main() {
         return validCount
     }
 
-    fun part2(input: List<String>): Int = 0
+    fun part2(input: List<String>): Long {
+        val (patterns, designs) = parseInput(input)
+
+        var totalWays = 0L
+        val memo = mutableMapOf<String, Long>()
+
+        for (design in designs) {
+            totalWays += countWaysToConstruct(design, patterns, memo)
+        }
+
+        return totalWays
+    }
 
     val testInput = readInput("day19/Day19_test")
     check(part1(testInput) == 6)
+    check(part2(testInput) == 16L)
 
     val input = readInput("day19/Day19")
     println(part1(input))
@@ -34,7 +46,7 @@ private fun parseInput(input: List<String>): Pair<Set<String>, List<String>> {
     return Pair(patterns, designs)
 }
 
-fun canConstructDesign(design: String, patterns: Set<String>, memo: MutableMap<String, Boolean>): Boolean {
+private fun canConstructDesign(design: String, patterns: Set<String>, memo: MutableMap<String, Boolean>): Boolean {
     if (design in memo) return memo[design]!!
     if (design.isEmpty()) return true
 
@@ -49,4 +61,20 @@ fun canConstructDesign(design: String, patterns: Set<String>, memo: MutableMap<S
     }
     memo[design] = false
     return false
+}
+
+private fun countWaysToConstruct(design: String, patterns: Set<String>, memo: MutableMap<String, Long>): Long {
+    if (design in memo) return memo[design]!!
+    if (design.isEmpty()) return 1L
+
+    var ways = 0L
+    for (pattern in patterns) {
+        if (design.startsWith(pattern)) {
+            val remaining = design.removePrefix(pattern)
+            ways += countWaysToConstruct(remaining, patterns, memo)
+        }
+    }
+
+    memo[design] = ways
+    return ways
 }
